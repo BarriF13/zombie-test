f17e31fce1073d80b6649c0b70c56eab81f7389b
Barri <info@bdesignstudio.co.uk>
2020-05-01 16:43:49 +0100

    success

--- app.js
+++ app.js
@@ -1,43 +1,54 @@
 //alert('hello')
-var character = {}
+// var test = {};
+var name2 = {};
+var character = {};
+var choice = {};
+
+const addName = (ev) => {
+  ev.preventDefault(); //to stop the form submitting 
+  name2 = {
+    testName: document.getElementById('name').value.toLowerCase()
+  }
+  console.log(name2.testName);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('btnName').addEventListener('click', addName);
+});
 
 const addCharacter = (ev) => {
   ev.preventDefault(); //to stop the form submitting 
-   character = {
+  character = {
     health: 5,
     strength: 0,
     stealth: 0,
-    name: document.getElementById('name').value.toLowerCase(),
+    // name: document.getElementById('name').value.toLowerCase(),
     charClass: document.getElementById('userCharacter').value.toLowerCase()
   }
- 
-  console.log(character.name);
   console.log(character.charClass);
-  //document.forms[0].reset();
-
-
 }
 
-
 document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('but').addEventListener('click', addCharacter);
+  document.getElementById('btnChar').addEventListener('click', addCharacter);
 });
+
 
 const addChoice = (ev) => {
   ev.preventDefault();
-  let test = {
-    
-   choice: document.getElementById('uChoice').value.toLowerCase()
-  }
-  console.log(test.choice);
-  console.log(character.name);
-  if (character.charClass === 'debugger' ){
-    if( test.choice === 'attack') {
-  document.getElementById('text').textContent =" It is working"}
-}
-}
+  choice = {
 
+    testChoice: document.getElementById('uChoice').value.toLowerCase()
+  }
+  console.log(choice.testChoice);
+  //run a function here --
+  if (character.charClass === 'debugger') {
+    if (choice.testChoice === 'attack') {
+      document.getElementById('text').textContent = " It is working"
+    }
+  }
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('attack').addEventListener('click', addChoice);
 });
+
