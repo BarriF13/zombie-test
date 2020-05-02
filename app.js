d40d75a2e8a070da722736bbba9320c77c192e7a
Barri <info@bdesignstudio.co.uk>
2020-05-02 13:01:15 +0100

    doctor and artist profile

--- app.js
+++ app.js
@@ -60,6 +60,10 @@ function game(){
     debuggerProfile()
   } else if (character.charClass === 'soldier') {
     soldierProfile()
+  } else if (character.charClass === 'artist') {
+    artistProfile()
+  } else if (character.charClass === 'doctor') {
+    doctorProfile()
   }
 }
 
@@ -81,6 +85,44 @@ function debuggerProfile (){
 }
 
 function soldierProfile (){
+  if (choice.playerChoice === 'attack') {
+    document.getElementById('charMsg').textContent = "Attacking the zombie with your bare hand "+ playerName.pName +", you slay that zombie!Victory! you just picked up a big axe and  off to kill them all.";
+    outcome ='win';
+    character.strength++;
+
+    document.getElementById('showPoints').textContent = "your health : "+character.health+"your strength : " +character.strength;
+
+  } else if (choice.playerChoice === "sneak" ||choice.playerChoice === "sneak by the zombie"){
+    document.getElementById('charMsg').textContent = "No no no " + playerName.pName + " , while you where sneaking out behind the zombie, accidentally dropped a can of diet coke...OH DEAR";
+    setTimeout(() => {
+      document.getElementById('charMsg').textContent = "And now zombie is jumping on you and bit your head off";
+    }, 2000);
+    outcome ='lose';
+    character.stealth++;
+
+    document.getElementById('showPoints').textContent = "your health : "+character.health+"your strength : " +character.strength;
+  }
+}
+function artistProfile (){
+  if (choice.playerChoice === 'attack') {
+    document.getElementById('charMsg').textContent = "Attacking the zombie with your bare hand "+ playerName.pName +", you slay that zombie!Victory! you just picked up a big axe and  off to kill them all.";
+    outcome ='win';
+    character.strength++;
+
+    document.getElementById('showPoints').textContent = "your health : "+character.health+"your strength : " +character.strength;
+
+  } else if (choice.playerChoice === "sneak" ||choice.playerChoice === "sneak by the zombie"){
+    document.getElementById('charMsg').textContent = "No no no " + playerName.pName + " , while you where sneaking out behind the zombie, accidentally dropped a can of diet coke...OH DEAR";
+    setTimeout(() => {
+      document.getElementById('charMsg').textContent = "And now zombie is jumping on you and bit your head off";
+    }, 2000);
+    outcome ='lose';
+    character.stealth++;
+
+    document.getElementById('showPoints').textContent = "your health : "+character.health+"your strength : " +character.strength;
+  }
+}
+function doctorProfile (){
   if (choice.playerChoice === 'attack') {
     document.getElementById('charMsg').textContent = "Attacking the zombie with your bare hand "+ playerName.pName +", you slay that zombie!Victory! you just picked up a big axe and  off to kill them all.";
     outcome ='win';
